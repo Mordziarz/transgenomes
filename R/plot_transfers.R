fb181575196e6e1c5e7c6ee4a65ee0b41e6b029f
Mordziarz <139069437+Mordziarz@users.noreply.github.com>
2026-01-08 10:52:37 +0100

    Update plot_transfers.R

--- R/plot_transfers.R
+++ R/plot_transfers.R
@@ -107,7 +107,7 @@ plot_transfers <- function(transfer_function_out,
     
     circlize::circos.axis(labels.cex = 0.4, minor.ticks = 1)
     
-  }, bg.border = NA, track.height = 0.1) # Grubszy pasek dla lepszej widoczności kolorów
+  }, bg.border = NA, track.height = 0.1)
   
   links_q_fix <- data.frame(
     chr = transfer_function_out$query_id,
@@ -125,12 +125,12 @@ plot_transfers <- function(transfer_function_out,
   graphics::legend(x = -1.3, y = -0.7, 
                    legend = c("MT -> PT", "PT -> MT", "Unknown"), 
                    fill = c(mt_to_pt_col, pt_to_mt_col, unknown_col), 
-                   title = "Transfer Direction", bty = "n", cex = 0.7)
+                   title = expression(bold("Transfer Direction"), bty = "n", cex = 0.7)
   
   graphics::legend(x = -1.3, y = -1.0, 
                    legend = c("Mitochondrion", "Plastid"), 
                    fill = c(mt_sector_col, pt_sector_col), 
-                   title = "Genomes", bty = "n", cex = 0.7)
+                   title = expression(bold("Genomes"), bty = "n", cex = 0.7)
   
   message("Plot created with maximized circle and side-by-side legends.")
 }
