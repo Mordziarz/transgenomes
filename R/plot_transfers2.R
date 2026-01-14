6c6fd214b9b0af4f17bd3a9092ac8a48924b754c
Mordziarz <139069437+Mordziarz@users.noreply.github.com>
2026-01-14 12:42:22 +0100

    Update plot_transfers2.R

--- R/plot_transfers2.R
+++ R/plot_transfers2.R
@@ -87,13 +87,13 @@ plot_transfers2 <- function(transfer_function_out,
     geom_segment(data = mt_info, 
                  aes(x = scale_pos(0, offset, total_mt_axis, normalization), 
                      xend = scale_pos(len, offset, total_mt_axis, normalization), 
-                     y = 5.5, yend = 5.5),
+                     y = 5.05, yend = 5.05),
                  linewidth = 4, color = mt_sector_col, lineend = "butt") +
     
     geom_segment(data = pt_info, 
                  aes(x = scale_pos(0, offset, total_pt_axis, normalization), 
                      xend = scale_pos(len, offset, total_pt_axis, normalization), 
-                     y = .5, yend = .5),
+                     y = .95, yend = .95),
                  linewidth = 4, color = pt_sector_col, lineend = "butt") +
     
     geom_text(data = mt_info, aes(x = scale_pos(len/2, offset, total_mt_axis, normalization), 
